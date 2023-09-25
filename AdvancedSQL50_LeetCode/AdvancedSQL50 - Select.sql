20dc5b67d190ceae36b1ea184d6513f85c64f6dc
Ramazan Yol <rmznyol@gmail.com>
2023-09-25 15:37:27 -0700

    update file format

--- AdvancedSQL50_LeetCode/AdvancedSQL50 - Select.sql
+++ AdvancedSQL50_LeetCode/AdvancedSQL50 - Select.sql
@@ -1,29 +1,26 @@
-1821. Find Customers With Positive Revenue this Year
+-- 1821. Find Customers With Positive Revenue this Year
 
 SELECT customer_id
 FROM Customers 
-WHERE year = 2021 and revenue > 0
+WHERE year = 2021 and revenue > 0;
 
-################################################################
-183. Customers Who Never Order
+-- 183. Customers Who Never Order
 
 SELECT c.name as Customers FROM Customers as c 
 LEFT JOIN Orders as o
 ON c.id = o.customerId
-WHERE o.id is null
+WHERE o.id is null;
 
-################################################################
-1873. Calculate Special Bonus
+-- 1873. Calculate Special Bonus
 SELECT employee_id,
 CASE
     WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary
     ELSE 0
 END as bonus
 FROM Employees
-ORDER BY employee_id
+ORDER BY employee_id;
 
-################################################################
-1398. Customers Who Bought Products A and B but Not C
+-- 1398. Customers Who Bought Products A and B but Not C
 
 SELECT DISTINCT o1.customer_id, c.customer_name
 FROM Orders as o1
@@ -35,10 +32,9 @@ WHERE o1.product_name = 'A' and o2.product_name = 'B' and o1.customer_id not in 
   SELECT customer_id
   FROM orders
   WHERE product_name = 'C'
-)
+);
 
-################################################################
-1112. Highest Grade For Each Student
+-- 1112. Highest Grade For Each Student
 
 WITH cte as (SELECT *, MAX(grade) OVER(PARTITION BY student_id) as mx
 FROM Enrollments
@@ -47,4 +43,4 @@ FROM Enrollments
 SELECT student_id, min(course_id) as course_id, grade
 FROM cte
 WHERE grade = mx 
-GROUP BY student_id, grade
+GROUP BY student_id, grade;

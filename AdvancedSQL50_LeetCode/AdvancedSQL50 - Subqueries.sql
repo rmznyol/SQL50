fec032e2dec947a4bb3a5db263ce1d9be54760d1
Ramazan Yol <rmznyol@gmail.com>
2023-09-25 15:39:33 -0700

    update file format

--- AdvancedSQL50_LeetCode/AdvancedSQL50 - Subqueries.sql
+++ AdvancedSQL50_LeetCode/AdvancedSQL50 - Subqueries.sql
@@ -1,26 +1,26 @@
-1350. Students With Invalid Departments
+-- 1350. Students With Invalid Departments
 
 SELECT s.id, s.name
 FROM Students as s
 LEFT JOIN Departments as d
 ON s.department_id = d.id
-WHERE d.id is Null
-################################################################################
-1303. Find the Team Size
+WHERE d.id is Null;
+
+-- 1303. Find the Team Size
 
 SELECT employee_id, COUNT(*) OVER(PARTITION BY team_id) as team_size
-FROM Employee
-################################################################################
-512. Game Play Analysis II
+FROM Employee;
+
+-- 512. Game Play Analysis II
 
 SELECT player_id, device_id
 FROM
     (SELECT player_id, device_id, 
     RANK() OVER(PARTITION BY player_id ORDER BY event_date ) as rnk
     FROM Activity) as t
-WHERE rnk = 1
-################################################################################
-184. Department Highest Salary
+WHERE rnk = 1;
+
+-- 184. Department Highest Salary
 SELECT Department, Employee, Salary
 FROM 
 (
@@ -30,10 +30,9 @@ FROM
   INNER JOIN Department as d
   ON e.departmentId = d.id
 ) as t
-WHERE t.Salary = t.mx
+WHERE t.Salary = t.mx;
 
-################################################################################
-1549. The Most Recent Orders for Each Product
+-- 1549. The Most Recent Orders for Each Product
 
 SELECT product_name, product_id, order_id, order_date
 FROM (
@@ -44,9 +43,9 @@ FROM (
   ON o.product_id = p.product_id 
 ) as t
 WHERE t.ord = 1
-ORDER BY product_name, product_id, order_id 
-################################################################################
-1532. The Most Recent Three Orders
+ORDER BY product_name, product_id, order_id;
+
+-- 1532. The Most Recent Three Orders
 
 WITH cte AS (
 SELECT c.customer_id, c.name as customer_name, o.order_id, o.order_date,
@@ -58,10 +57,9 @@ ON c.customer_id = o.customer_id)
 SELECT  customer_name, customer_id, order_id, order_date
 FROM cte
 WHERE rnk <= 3 
-ORDER BY customer_name, customer_id, order_date DESC
+ORDER BY customer_name, customer_id, order_date DESC;
 
-################################################################################
-1831. Maximum Transaction Each Day
+-- 1831. Maximum Transaction Each Day
 
 SELECT transaction_id
 FROM (
@@ -69,4 +67,4 @@ FROM (
   FROM Transactions
 ) as t
 WHERE t.amount = t.mx
-ORDER BY transaction_id
+ORDER BY transaction_id;

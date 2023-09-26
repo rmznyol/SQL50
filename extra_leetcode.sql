290898bd76a7948dd8c161acfad49a0c8f857490
Ramazan Yol <rmznyol@gmail.com>
2023-09-26 12:58:08 -0700

    update file format

--- extra_leetcode.sql
+++ extra_leetcode.sql
@@ -1,4 +1,4 @@
-262. Trips and Users (HARD)
+-- 262. Trips and Users (HARD)
 
 SELECT t.request_at as Day, ROUND((SUM(t.status = 'cancelled_by_driver') + SUM(t.status = 'cancelled_by_client'))/COUNT(t.status),2) as 'Cancellation Rate'
 FROM Trips as t
@@ -8,9 +8,9 @@ INNER JOIN Users as u2
 ON t.driver_id = u2.users_id
 WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
 AND u1.banned = 'No' AND u2.banned = 'No'
-GROUP BY t.request_at
-#################################################################################
-569. Median Employee Salary (HARD)
+GROUP BY t.request_at;
+
+-- 569. Median Employee Salary (HARD)
 
 SELECT id, company, salary
 FROM (
@@ -18,5 +18,4 @@ FROM (
   RANK() OVER(PARTITION BY company ORDER   BY Salary,id) as rnk
   FROM Employee
 ) as t
-WHERE rnk BETWEEN cnt / 2 AND cnt/2 + 1 
-#################################################################################
+WHERE rnk BETWEEN cnt / 2 AND cnt/2 + 1; 

c383333fb253ae6b9dc4b349c901aa73c83b5070
Ramazan Yol <rmznyol@gmail.com>
2023-10-01 10:26:44 -0700

    add solution for New Companies

--- HackerRank/HackerRank_Advanced_Select.sql
+++ HackerRank/HackerRank_Advanced_Select.sql
@@ -36,3 +36,15 @@ FROM BST as b1
 LEFT JOIN BST as b2
 ON b1.N = b2.P
 ORDER BY b1.N
+
+-- New Companies
+SELECT c.company_code, c.founder, 
+COUNT(DISTINCT e.lead_manager_code),
+COUNT(DISTINCT e.senior_manager_code), 
+COUNT(DISTINCT e.manager_code),
+COUNT(DISTINCT e.employee_code)
+FROM Company as c
+INNER JOIN Employee as e 
+ON c.company_code = e.company_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;

fcf10fe69ea1cd53b69710783ba6bf3ec9ca423b
Ramazan Yol <rmznyol@gmail.com>
2023-10-10 14:14:26 -0700

    add solution forTop Earners

--- HackerRank/HackerRank_Aggregation.sql
+++ HackerRank/HackerRank_Aggregation.sql
@@ -1,31 +1,38 @@
 -- Revising Aggregations - The Count Function
 SELECT COUNT(NAME)
 FROM CITY
-WHERE POPULATION > 100000
+WHERE POPULATION > 100000;
 
 -- Revising Aggregations - The Sum Function
 SELECT SUM(POPULATION)
 FROM CITY
-WHERE DISTRICT = 'California'
+WHERE DISTRICT = 'California';
 
 -- Averages
 SELECT AVG(POPULATION)
 FROM CITY
-WHERE DISTRICT = 'California'
+WHERE DISTRICT = 'California';
 
 -- Average Population
 SELECT ROUND(AVG(POPULATION))
-FROM CITY
+FROM CITY;
 
 -- Japan Population
 SELECT SUM(POPULATION)
 FROM CITY
-WHERE COUNTRYCODE = 'JPN'
+WHERE COUNTRYCODE = 'JPN';
 
 -- Population Density Difference
 SELECT MAX(POPULATION) - MIN(POPULATION)
-FROM CITY
+FROM CITY;
 
 -- The Blunder
 SELECT CEIL(AVG(SALARY - CAST(REPLACE(CAST(SALARY AS CHAR(50)),'0', '') AS UNSIGNED)))
-FROM EMPLOYEES
+FROM EMPLOYEES;
+
+-- Top Earners
+SELECT MAX(salary*months), COUNT(*)
+FROM employee
+WHERE salary*months = (
+SELECT MAX(salary*months)
+From employee);

5d52b57f4e998cac4d83b76de0621ce87f5bb30d
Ramazan Yol <rmznyol@gmail.com>
2023-10-17 10:09:25 -0700

    add solution for Top Competitors

--- HackerRank/HackerRank_Basic_Join.sql
+++ HackerRank/HackerRank_Basic_Join.sql
@@ -25,3 +25,17 @@ FROM Students
 INNER JOIN Grades
 ON Grades.Min_Mark <= Students.Marks AND Students.Marks <= Grades.Max_Mark
 ORDER BY Grades.grade DESC, Students.Name, Students.Marks;
+
+-- Top Competitors
+SELECT s.hacker_id, h.name
+FROM Submissions as s
+    INNER JOIN Challenges as c
+        ON s.challenge_id = c.challenge_id
+    INNER JOIN Difficulty as d
+        ON d.difficulty_level = c.difficulty_level
+    INNER JOIN Hackers as h
+        ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score
+GROUP BY s.hacker_id, h.name
+HAVING COUNT(DISTINCT(s.challenge_id)) > 1
+ORDER BY COUNT(DISTINCT(s.challenge_id)) DESC, s.hacker_id;

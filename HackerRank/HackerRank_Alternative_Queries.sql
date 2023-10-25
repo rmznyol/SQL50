abbfa4f8ea339d6b22e0b98f0707aa71dd347ce0
Ramazan Yol <rmznyol@gmail.com>
2023-10-24 20:36:48 -0700

    add solution for Draw The Triangle 1

---
+++ HackerRank/HackerRank_Alternative_Queries.sql
@@ -0,0 +1,16 @@
+-- Draw The Triangle
+
+WITH RECURSIVE cte  AS
+(
+SELECT 1 as cnt, CAST('*' AS CHAR(50)) as res
+union all
+SELECT cnt+1, concat(res,' ','*') from cte where cnt < 20
+)
+SELECT res FROM cte
+ORDER BY cnt DESC;  
+
+-- This is listed as an easy problem 
+-- However, in my opinion it is a hard problem
+-- Limiting the recursion on length of the string
+-- prompts an error: 'ERROR 1406 (22001): Data too long for column 'str' at row 1'
+-- See documentation for help: https://dev.mysql.com/doc/refman/8.0/en/with.html
